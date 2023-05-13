4281c1966f38427163e0f898d2521b3cf8822597
Pedro Miguel Pereira <78454877+pedromcdp@users.noreply.github.com>
2023-05-13 19:19:07 +0100

    Update corsOptions.js

--- utils/corsOptions.js
+++ utils/corsOptions.js
@@ -1,4 +1,4 @@
 exports.corsOptions = {
-  origin: ['https://www.storyarc.pt/', 'http://localhost:3000'],
+  origin: ['https://www.storyarc.pt/', 'http://localhost:3000', 'https://build-pmcdp.vercel.app'],
   optionsSuccessStatus: 200,
 };
